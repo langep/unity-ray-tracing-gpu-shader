292219a41c76726d4673f42646cd72939c71fd55
Patrick Lange <mail@langep.com>
2019-10-27 15:31:55 -0700

    Initial setup.

---
+++ Assets/Scripts/RayTracingMaster.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayTracingMaster : MonoBehaviour
+{
+
+    [SerializeField] private ComputeShader _shader;
+    private RenderTexture _target;
+
+    // Automatically called by Unity when Camera finished rendering
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Render(destination);
+    }
+
+    private void Render(RenderTexture destination)
+    {
+        // Make sure we have a render target of appropriate dimension
+        InitRenderTexture();
+
+        // Setup the target in the shader
+        _shader.SetTexture(0, "Result", _target);
+
+        // We will thread groups of size (threadGroupSizeX x threadGroupSizeY) pizels
+        float threadGroupSizeX = 8.0f;
+        float threadGroupSizeY = 8.0f;
+        int threadGroupsX = Mathf.CeilToInt(Screen.width / threadGroupSizeX);
+        int threadGroupsY = Mathf.CeilToInt(Screen.height / threadGroupSizeY);
+
+        // Dispatch the shader
+        _shader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+
+        // Blit the result texture to the screen
+        Graphics.Blit(_target, destination);
+    }
+
+    // Initializes the render texture with the correct dimensions
+    private void InitRenderTexture()
+    {
+        if (_target == null || _target.height != Screen.height || _target.width != Screen.width)
+        {
+            // We are either uninitialized or have mismatching dimensions
+
+            // Release the current (mismatching) target if we have one
+            if (_target != null)
+            {
+                _target.Release();
+            }
+
+            // Create a new render target
+            _target = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            _target.enableRandomWrite = true;
+            _target.Create();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# Unity - GPU Ray Tracing in Unity
+
+## About
+
+This is a project to learn about Compute Shaders and Ray Tracing in Unity. I am following
+the [great article](http://blog.three-eyed-games.com/2018/05/03/gpu-ray-tracing-in-unity-part-1/) by
+David Kuri on the topic.
+
+## Project Organization
+
+Each stage of the project will be available as individual tag.
+
+| Tag                        | Description                                                    |
+| -------------------------- | -------------------------------------------------------------- |
+| 01-setup-raytracing-master | Create the controller script which invokes the compute shader. |
+
+## 01-setup-raytracing-master
+
+This is the output after setting up the `RayTracingMaster.cs` script to invoke Unity's default compute shader.
+
+![Unity Default Compute Shader](./Documentation/Visualization/01-setup-raytracing-master.PNG "Unity Default Compute Shader")
